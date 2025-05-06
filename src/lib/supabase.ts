a8dc49c5aac61583a1476ded757361069cf78483
Gabriel Paiva <paivajosegabriel@gmail.com>
2025-05-06 12:00:45 -0300

    supabase error check

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,9 @@
-import { createClient } from '@supabase/supabase-js';
+import "react-native-url-polyfill/auto";
+import { createClient } from "@supabase/supabase-js";
 
 //valores do projeto no Supabase:
-const supabaseUrl = 'https://bnlykckogdmetkpxlyvp.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJubHlrY2tvZ2RtZXRrcHhseXZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5NjkyNjUsImV4cCI6MjA1OTU0NTI2NX0.IB_z3VZZkCZndsMLH8bPPvMSGflyesE4E3JrNLonSvw';
+const supabaseUrl = "https://bnlykckogdmetkpxlyvp.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJubHlrY2tvZ2RtZXRrcHhseXZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5NjkyNjUsImV4cCI6MjA1OTU0NTI2NX0.IB_z3VZZkCZndsMLH8bPPvMSGflyesE4E3JrNLonSvw";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
